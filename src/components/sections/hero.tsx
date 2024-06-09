7537dea96ca74495a3483974bf2629ac3d63e51e
Kai <kkai.chien0813@gmail.com>
2024-06-08 17:41:15 -0700

    chore: adjust the header design

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Link as Scroll } from "react-scroll";
 
 import Header from "../ui/header";
 import Button from "../ui/button";
 import Logo from "../../../public/logo.svg";
 import { HeroHighlightText } from "../heroHighlight";
 import { Boxes } from "../ui/background-boxes";
+import Link from "next/link";
 
 export default function Hero() {
 	return (
@@ -12,7 +14,6 @@ export default function Hero() {
 			<Header />
 			<div className="h-screen relative w-full overflow-hidden  flex flex-col items-center justify-center rounded-lg">
 				<div className="absolute inset-0 w-full h-full  z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-
 				<Boxes />
 				<Image
 					src={Logo}
