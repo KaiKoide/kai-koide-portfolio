810cd45fdef1815789c17df6a305234900dfa60b
Kai <kkai.chien0813@gmail.com>
2024-06-11 23:12:27 -0700

    feat: switch the position of contact icons and text

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -43,8 +43,8 @@ export default function Contact() {
 			className="w-4/5 m-auto mt-20 lg:mt-40 flex flex-col justify-center items-center gap-16"
 		>
 			<h2 className="uppercase text-5xl">contact me</h2>
-			<TypewriterEffectSmooth words={words} />
 			<ContactList contacts={contactsData} />
+			<TypewriterEffectSmooth words={words} />
 		</div>
 	);
 }
