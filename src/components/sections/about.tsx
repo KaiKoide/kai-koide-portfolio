dd15c657ed1d0af9166ffdc336267346036337b0
Kai <kkai.chien0813@gmail.com>
2024-06-25 22:50:34 -0700

    feat: implement responsive design on about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -14,16 +14,24 @@ export default function About() {
 			id="about"
 			className="w-4/5 flex flex-col items-center justify-center m-auto mt-20 lg:mt-40 gap-y-10"
 		>
-			<h2 className="text-6xl uppercase">about me</h2>
-			<div className="flex">
-				<div className="flex flex-col gap-y-10">
+			<h2 className="text-4xl md:text-5xl lg:text-6xl uppercase">about me</h2>
+			<div className="flex flex-col lg:flex-row justify-center items-center">
+				<Image
+					src={Profile}
+					alt="profile image"
+					width={300}
+					height={300}
+					className="lg:order-2"
+				/>
+				<div className="flex flex-col gap-y-10 lg:order-1">
 					<EffectedText words={aboutData.description1} />
 					<EffectedText words={aboutData.description2} />
 				</div>
-				<Image src={Profile} alt="profile image" width={300} height={300} />
 			</div>
 
-			<h3 className="uppercase text-5xl mt-20">skills</h3>
+			<h3 className="uppercase text-3xl md:text-4xl lg:text-5xl mt-20">
+				skills
+			</h3>
 			<div className="flex flex-col gap-y-10">
 				{skillTypes.map((type) => (
 					<SkillIcons key={type} type={type} />
