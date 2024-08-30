00e56b3d70a014019ccb620ef44c0d5c59caa068
Kai Koide <144519746+KaiKoide@users.noreply.github.com>
2024-08-29 17:31:07 -0700

    feat: adjust padding on detail page (#8)

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -7,6 +7,7 @@ import { projects } from "@/data/projectData";
 import type { ParamProp } from "@/util/types/paramType";
 import { EmblaCarousel } from "@/components/ui/carousel";
 import Badge from "@/components/ui/badge";
+import Footer from "@/components/sections/footer";
 
 export default function ProjectsPage({ params }: ParamProp) {
 	const project = projects.find(
@@ -28,44 +29,47 @@ export default function ProjectsPage({ params }: ParamProp) {
 	}
 
 	return (
-		<div className="w-3/4 m-auto">
-			<Link href={"/"}>
-				<div className="flex items-center gap-2 py-5">
-					<PiArrowFatLeftFill />
-					<h4 className="capitalize font-bold">go back</h4>
+		<>
+			<div className="w-full lg:w-3/4 mx-auto px-4 sm:px-0">
+				<Link href={"/"}>
+					<div className="flex items-center gap-2 py-5">
+						<PiArrowFatLeftFill />
+						<h4 className="capitalize font-bold">go back</h4>
+					</div>
+				</Link>
+				<h2 className="m-auto text-center text-4xl font-extrabold py-10">
+					{project?.title}
+				</h2>
+				<EmblaCarousel project={project} />
+				<p className="text-justify">{project?.overview}</p>
+				<h3 className="capitalize font-bold text-2xl pt-10">tech stack</h3>
+				<div className="flex flex-wrap gap-3 w-full my-5">
+					{project?.tech.map((item, index) => (
+						<Badge message={item} key={index} />
+					))}
 				</div>
-			</Link>
-			<h2 className="m-auto text-center text-4xl font-extrabold py-10">
-				{project?.title}
-			</h2>
-			<EmblaCarousel project={project} />
-			<p>{project?.overview}</p>
-			<h3 className="capitalize font-bold text-2xl pt-10">tech stack</h3>
-			<div className="flex flex-wrap gap-3 w-full my-5">
-				{project?.tech.map((item, index) => (
-					<Badge message={item} key={index} />
-				))}
-			</div>
-			<div className="pt-5 pb-10 flex gap-4 justify-center">
-				<a
-					href={project?.github}
-					target="_blank"
-					rel="noopener noreferrer"
-					className="block"
-				>
-					<FaGithub size={50} />
-				</a>
-				{project.website && (
+				<div className="pt-5 pb-10 flex gap-4 justify-center">
 					<a
-						href={project?.website}
+						href={project?.github}
 						target="_blank"
 						rel="noopener noreferrer"
 						className="block"
 					>
-						<CgBrowser size={50} />
+						<FaGithub size={50} />
 					</a>
-				)}
+					{project.website && (
+						<a
+							href={project?.website}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="block"
+						>
+							<CgBrowser size={50} />
+						</a>
+					)}
+				</div>
 			</div>
-		</div>
+			<Footer />
+		</>
 	);
 }
