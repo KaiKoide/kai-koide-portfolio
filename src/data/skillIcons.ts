378c66b4c1068f50b0e0bf64cb663f12444f250e
Kai <kkai.chien0813@gmail.com>
2024-09-09 16:11:06 -0700

    feat: add Next.js to skills

--- src/data/skillIcons.ts
+++ src/data/skillIcons.ts
@@ -26,6 +26,7 @@ import {
 	SiPostman,
 	SiFigma,
 	SiBun,
+	SiNextdotjs,
 } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { DiMysql } from "react-icons/di";
@@ -60,31 +61,36 @@ export const frontEnd: Skill[] = [
 	},
 	{
 		id: 6,
+		icon: SiNextdotjs,
+		text: "Next.js",
+	},
+	{
+		id: 7,
 		icon: SiRedux,
 		text: "Redux",
 	},
 	{
-		id: 7,
+		id: 8,
 		icon: SiAstro,
 		text: "Astro",
 	},
 	{
-		id: 8,
+		id: 9,
 		icon: SiVite,
 		text: "Vite",
 	},
 	{
-		id: 9,
+		id: 10,
 		icon: RiTailwindCssFill,
 		text: "Tailwind CSS",
 	},
 	{
-		id: 10,
+		id: 11,
 		icon: SiMui,
 		text: "MUI",
 	},
 	{
-		id: 11,
+		id: 12,
 		icon: SiShadcnui,
 		text: "shadcn/ui",
 	},
