08369ea5fc7a388fbbfe9e67a5c3813798ca73af
Kai <kkai.chien0813@gmail.com>
2024-09-11 11:23:03 -0700

    feat: introduce animation to contact section

--- src/components/contactList.tsx
+++ src/components/contactList.tsx
@@ -1,21 +1,42 @@
 import type { ContactType } from "@/util/types/contactType";
+import { FloatingDock } from "./ui/floating-dock";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 interface ContactTypeProps {
 	contacts: ContactType[];
 }
 
 const ContactList: React.FC<ContactTypeProps> = ({ contacts }) => {
+	const links = [
+		{
+			title: "GitHub",
+			icon: (
+				<FaGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "https://github.com/KaiKoide",
+		},
+
+		{
+			title: "linkedIn",
+			icon: (
+				<FaLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "https://www.linkedin.com/in/kai-koide-7b9006292/",
+		},
+		{
+			title: "E-mail",
+			icon: (
+				<IoMdMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "mailto:k.koide.van24@gmail.com",
+		},
+	];
 	return (
-		<div className="flex gap-10">
-			{contacts.map((contact) => (
-				<div key={contact.id}>
-					<div className="tooltip" data-tip={contact.name}>
-						<a href={contact.href} target="_blank" rel="noopener noreferrer">
-							<contact.icon size={50} />
-						</a>
-					</div>
-				</div>
-			))}
+		<div className="flex gap-5">
+			<div className="flex items-center justify-center h-5 w-full">
+				<FloatingDock items={links} />
+			</div>
 		</div>
 	);
 };
